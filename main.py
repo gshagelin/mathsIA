6ff3f6375bb1946bfbd704850bde4de06aa4823c
Simon <kattnoskanal@gmail.com>
2023-10-25 19:29:40 +0200

    new

--- main.py
+++ main.py
@@ -2,32 +2,33 @@ import random
 import numpy as np
 
 primeList = []
+
+M = 1882970215008405644082828404422446644402468008.0
+lowestInt = int(np.ceil(np.sqrt(M)))
+
+
 def generate_prime():
-    for int in range(10000, 20000, 1):
+    for primeInt in range(lowestInt, lowestInt + 100, 1):
         intPrime = True
-        limit = int
-        for i in range(2, limit - 1, 1):
-            if int % i == 0:
-                #  print(str(int) + ' ' +str(i))
-                intPrime = False
-        if intPrime:
-            primeList.append(int)
+        primeIntFloat = float(primeInt)
+        limit = int(np.ceil(np.sqrt(primeIntFloat))) + 1
+        if primeInt % 2 == 0:
+            intPrime = False
+        else:
+            for i in range(3, limit, 2):
+                if primeInt % i == 0:
+                    intPrime = False
+                    break
+            if intPrime:
+                primeList.append(primeInt)
+
 
 generate_prime()
-q = random.choice(primeList) #secret
-p = random.choice(primeList) #secret
+q = random.choice(primeList)  # secret
+p = random.choice(primeList)  # secret
+print(str(p))
+print(str(q))
 while p == q:
     p = random.choice(primeList)
-
-print(q)
-print(p)
-
-n = p * q #public key
-
-lambdaP = p - 1
-lambdaQ = q - 1
-
-lambdaN = np.lcm(lambdaP, lambdaQ)    #secret
-
 
 
